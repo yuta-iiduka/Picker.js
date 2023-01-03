827e78e6c075d469d9c0d15415f184bd265cfa76
yuta-iiduka <ota12041204@gmail.com>
2023-01-03 17:43:29 +0900

    first commit

---
+++ js/collider.js
@@ -0,0 +1,277 @@
+console.log("collider.js is called.");
+// ■Colliderクラスは以下の機能を提供します
+//① 移動機能
+//② リサイズ機能
+//③ ①②のONOFFボタン
+// ■依存：jquery-3.6.0.min.js
+
+class Collider{
+	// 共通カウンタ
+	static cnt = 0;
+	// 全Colliderオブジェクトのリスト
+	static list = [];
+	// アクティブなColliderオブジェクト
+	static active_collider = null;
+	
+	//コンストラクタ
+	//TODO：カプセル化
+	constructor(){
+		this.x = 0;
+		this.y = 0;
+		this.kind = "";
+		this.id   = Collider.cnt;
+		this.body = $("body");;
+		this.jquery_obj = null;
+		this.active = false;		//Colliderの有効・無効
+		this.move = false;			//移動判定
+		this.resize_l = false;		//X方向リサイズ判定
+		this.resize_r = false;		//X方向リサイズ判定
+		this.resize_y = false;		//Y方向リサイズ判定
+		Collider.list.push(this);
+		Collider.cnt++;
+	}
+	
+	//説明：JQueryオブジェクトのセッター
+	//引数：JQueryオブジェクト
+	//戻値：自インスタンス
+	set_jquery_obj(jquery_obj){
+		this.jquery_obj = jquery_obj;
+		return this;
+	}
+	
+	//説明：移動イベントを与える関数
+	//引数：なし
+	//戻値：自インスタンス
+	set_collider(){
+		let self = this;
+		let wid  = 0;
+		let hei  = 0;
+		this.body.css("position","relative").css("height","100vh").css("width","100vw");
+		this.jquery_obj.css("position","fixed");
+		
+		// PC
+		this.jquery_obj.mousedown(function(e){
+			if(self.active == true){
+				self.move = true;
+				wid = e.pageX - self.jquery_obj.offset()["left"];
+				hei = e.pageY - self.jquery_obj.offset()["top"];
+			}
+		});
+		this.body.mousemove(function(e){
+			if(self.move == true){
+				e.preventDefault();
+				self.x = e.pageX - wid; //self.jquery_obj.width() / 2;
+				self.y = e.pageY - hei;  //self.jquery_obj.height() / 2 ;
+				self.jquery_obj.offset({"top": self.y ,"left": self.x});
+				window.getSelection().removeAllRanges();
+			}
+		});
+		this.body.mouseup(function(e){
+			self.move = false;
+		});
+		this.jquery_obj.mouseup(function(e){
+			self.move = false;
+		});
+		
+		// Android
+		this.jquery_obj.bind("touchstart",function(e){
+			if(self.active == true){
+				self.move = true;
+				wid = e.changedTouches[0].pageX - self.jquery_obj.offset()["left"];
+				hei = e.changedTouches[0].pageY - self.jquery_obj.offset()["top"];
+			}
+		});
+		this.body.bind("touchmove",function(e){
+			if(self.move == true){
+				self.x = e.changedTouches[0].pageX - wid; //self.jquery_obj.width() / 2;
+				self.y = e.changedTouches[0].pageY - hei; //self.jquery_obj.height() / 2;
+				self.jquery_obj.offset({"top": self.y ,"left": self.x})
+				window.getSelection().removeAllRanges();
+			}
+		});
+		this.body.bind("touchend",function(e){
+			self.move = false;
+		});
+		this.jquery_obj.bind("touchend",function(e){
+			self.move = false;
+		});
+		return this;
+	}
+	
+	//説明：リサイズイベントを与える関数
+	//引数：なし
+	//戻値：自インスタンス
+	set_frame(){
+		let self = this;
+		//self.jquery_obj.offset(),width(),height() は毎回取得する
+		//TODO:ここはかなり冗長な書き方なので改善が必要
+		
+		// PC
+		this.jquery_obj.mousemove(function(e){
+			if(self.active == true){
+				if(e.pageY >= self.jquery_obj.offset()["top"] - 1 && e.pageY <= self.jquery_obj.offset()["top"] + 5){
+					self.jquery_obj.css("cursor","move");
+				}else if(e.pageY >= self.jquery_obj.offset()["top"] + self.jquery_obj.height() - 5 && e.pageY <= self.jquery_obj.offset()["top"] + self.jquery_obj.height()+1){
+					self.jquery_obj.css("cursor","ns-resize");
+				}else if(e.pageX >= self.jquery_obj.offset()["left"] - 1 && e.pageX <= self.jquery_obj.offset()["left"] + 5){
+					self.jquery_obj.css("cursor","ew-resize");
+				}else if(e.pageX >= self.jquery_obj.offset()["left"] + self.jquery_obj.width() - 5 && e.pageX <= self.jquery_obj.offset()["left"] + self.jquery_obj.width()+1){
+					self.jquery_obj.css("cursor","ew-resize");
+				}else{
+					self.jquery_obj.css("cursor","default");
+				}
+			}
+		});
+		this.body.mousemove(function(e){
+			//リサイズモードだった場合
+			if(self.resize_y == true){
+				self.jquery_obj.height(e.pageY - self.jquery_obj.offset()["top"]);
+				window.getSelection().removeAllRanges();
+			}
+			if(self.resize_l == true){
+				self.jquery_obj.width(self.jquery_obj.width() + self.jquery_obj.offset()["left"] - e.pageX )
+								.offset({
+									"top" :self.jquery_obj.offset()["top"],
+									"left":e.pageX
+								})
+				window.getSelection().removeAllRanges();
+			}
+			if(self.resize_r == true){
+				self.jquery_obj.width(e.pageX - self.jquery_obj.offset()["left"]);
+				window.getSelection().removeAllRanges();
+			}
+		});
+		this.body.mousedown(function(e){
+			if(self.active == true){
+				if(e.pageY >= self.jquery_obj.offset()["top"] - 1 && e.pageY <= self.jquery_obj.offset()["top"] + 5){
+					//self.resize_y = true;
+				}else if(e.pageY >= self.jquery_obj.offset()["top"] + self.jquery_obj.height() - 5 && e.pageY <= self.jquery_obj.offset()["top"] + self.jquery_obj.height() + 1){
+					if(e.pageX >= self.jquery_obj.offset()["left"] && e.pageX <= self.jquery_obj.offset()["left"] + self.jquery_obj.width()){
+						self.resize_y = true;
+						self.move = false;
+					}
+				}
+				if(e.pageX >= self.jquery_obj.offset()["left"] - 1 && e.pageX <= self.jquery_obj.offset()["left"] + 5){
+					if(e.pageY >= self.jquery_obj.offset()["top"] && e.pageY <= self.jquery_obj.offset()["top"] + self.jquery_obj.height()){
+						self.resize_l = true;
+						self.move = false;
+					}
+				}else if(e.pageX >= self.jquery_obj.offset()["left"] + self.jquery_obj.width() - 5 && e.pageX <= self.jquery_obj.offset()["left"] + self.jquery_obj.width() + 1){
+					if(e.pageY >= self.jquery_obj.offset()["top"] && e.pageY <= self.jquery_obj.offset()["top"] + self.jquery_obj.height()){
+						self.resize_r = true;
+						self.move = false;
+					}
+				}
+			}
+		});
+		this.body.mouseup(function(e){
+			self.resize_r = false;
+			self.resize_l = false;
+			self.resize_y = false;
+		});
+		
+		// Android
+		this.jquery_obj.bind("touchstart",function(e){
+			let epageX = e.changedTouches[0].pageX;
+			let epageY = e.changedTouches[0].pageY;
+			if(self.active == true){
+				if(epageY >= self.jquery_obj.offset()["top"] - 1 && epageY <= self.jquery_obj.offset()["top"] + 5){
+					//self.resize_y = true;
+				}else if(epageY >= self.jquery_obj.offset()["top"] + self.jquery_obj.height() - 5 && epageY <= self.jquery_obj.offset()["top"] + self.jquery_obj.height() + 1){
+					self.resize_y = true;
+					self.move = false;
+				}
+				if(epageX >= self.jquery_obj.offset()["left"] - 1 && epageX <= self.jquery_obj.offset()["left"] + 5){
+					self.resize_l = true;
+					self.move = false;
+				}else if(epageX >= self.jquery_obj.offset()["left"] + self.jquery_obj.width() - 5 && epageX <= self.jquery_obj.offset()["left"] + self.jquery_obj.width() + 1){
+					self.resize_r = true;
+					self.move = false;
+				}
+			}
+		});
+		this.body.bind("touchmove",function(e){
+			let epageX = e.changedTouches[0].pageX;
+			let epageY = e.changedTouches[0].pageY;
+			//リサイズモードだった場合
+			if(self.resize_y == true){
+				self.jquery_obj.height(epageY - self.jquery_obj.offset()["top"]);
+				window.getSelection().removeAllRanges();
+			}
+			if(self.resize_l == true){
+				self.jquery_obj.width(self.jquery_obj.width() + self.jquery_obj.offset()["left"] - epageX )
+								.offset({
+									"top" :self.jquery_obj.offset()["top"],
+									"left":epageX
+								})
+				window.getSelection().removeAllRanges();
+			}
+			if(self.resize_r == true){
+				self.jquery_obj.width(epageX - self.jquery_obj.offset()["left"]);
+				window.getSelection().removeAllRanges();
+			}
+		});
+		this.body.bind("touchend",function(e){
+			self.resize_r = false;
+			self.resize_l = false;
+			self.resize_y = false;
+		});
+		return this;
+	}
+	
+	//説明：Colliderオブジェクトリストのゲッター（Colliderグループ検索）
+	//引数：文字列
+	//戻値：Colliderオブジェクト
+	get_collider_list_by_kind(kind){
+		let target = [];
+		for(let i=0; i<Collider.list.length; i++){
+			if(Collider.list[i].kind == kind){
+				target.push(Collider.list[i]);
+			}
+		}
+		return target;
+	}
+	
+	//説明：Colliderオブジェクトのゲッター（ColliderID検索）
+	//引数：文字列
+	//戻値：Colliderオブジェクト	
+	get_collider_by_id(id){
+		let target = null;
+		for(let i=0; i<Collider.list.length; i++){
+			if(Collider.list[i].id == id){
+				target = Collider.list[i];
+			}
+		}
+		return target;
+	}
+	
+	//説明：Collider機能のONOFF
+	//引数：なし
+	//戻値：なし
+	change_mode(){
+		if(this.active == true){
+			this.active = false;
+			this.jquery_obj.removeClass("border border-primary");
+		}else{
+			this.active = true;
+			this.jquery_obj.addClass("border border-primary");
+		}
+	}
+	
+	//説明：Collider機能のONOFFボタン
+	//引数：なし
+	//戻値：なし
+	set_button(){
+		let self = this;
+		//this.jquery_obj.attr("contenteditable","true");
+		this.jquery_obj.prepend('<span><i id="settings' + this.id + '" class="bi bi-gear-fill"></i></span>');
+		let btn = $("#settings" + this.id)
+		btn.css("cursor","pointer")
+			.css("position","relative")
+			//.offset({"top":btn.offset()["top"],"left":btn.offset()["left"]+this.jquery_obj.width() - 16});
+		btn.click(function(){
+			self.change_mode()
+		});
+		return this;
+	}
+}
---
+++ js/tree.js
@@ -0,0 +1,239 @@
+console.log("tree.js is called.");
+// ■Treeクラスは以下の機能を提供します
+//① 親子関係
+//② ルートの取得機能
+//③ グループや名前での検索
+// ■依存：jquery-3.6.0.min.js
+class Tree{
+	// 共通カウンタ
+	static cnt = 0;
+	// 全Treeオブジェクトのリスト
+	static list = [];
+	// アクティブなTreeオブジェクト
+	static active_tree = null;
+	// ルート
+	static route = [];
+	
+	//コンストラクタ
+	//TODO：カプセル化
+	constructor(){
+		this.x = 0;
+		this.y = 0;
+		this.id = Tree.cnt;
+		this.group = "";
+		this.name = "";
+		this.parent = null;
+		this.child = [];
+		this.original = null;
+		Tree.list.push(this);
+		Tree.cnt++;
+	}
+	
+	//説明：親Treeのセッター
+	//引数：Treeオブジェクト
+	//戻値：自インスタンス
+	set_parent(t){
+		if(t != this){
+			this.parent = t;
+			if(t.child.includes(this)){
+				t.set_child(this);
+			}
+		}else{
+			console.log("Tree id:" + t.id + " Treeオブジェクトは自身を親にすることはできません。");
+		}
+		return this;
+	}
+	
+	//説明：子Treeのセッター
+	//引数：Treeオブジェクト
+	//戻値：自インスタンス
+	set_child(t){
+		if(this.child.includes(t)){
+			console.log("Tree id:" + t.id + " 同じTreeオブジェクトは子にすることはできません。");
+		}else{
+			this.child.push(t);
+		}
+		if (t.parent = null){
+			t.set_parent(this);
+		}
+		return this;
+	}
+
+	//説明：Treeオブジェクトリストのゲッター（Tree名検索）
+	//引数：文字列
+	//戻値：Treeオブジェクト	
+	get_tree_list_by_name(name){
+		let target = [];
+		for(let i=0; i<Tree.list.length; i++){
+			if(Tree.list[i].name == name){
+				target.push(Tree.list[i]);
+			}
+		}
+		return target;
+	}
+	
+	//説明：Treeオブジェクトリストのゲッター（Treeグループ検索）
+	//引数：文字列
+	//戻値：Treeオブジェクト
+	get_tree_list_by_group(group){
+		let target = [];
+		for(let i=0; i<Tree.list.length; i++){
+			if(Tree.list[i].group == group){
+				target.push(Tree.list[i]);
+			}
+		}
+		return target;
+	}
+	
+	//説明：Treeオブジェクトのゲッター（TreeID検索）
+	//引数：文字列
+	//戻値：Treeオブジェクト	
+	get_tree_by_id(id){
+		let target = null;
+		for(let i=0; i<Tree.list.length; i++){
+			if(Tree.list[i].id == id){
+				target = Tree.list[i];
+			}
+		}
+		return target;
+	}
+	
+	//説明：始祖Treeオブジェクトのゲッター
+	//引数：なし
+	//戻値：Treeオブジェクト	
+	get_original(){
+		let target = null;
+		if(this.parent == null){
+			target = this;
+		}else{
+			target = this.parent.get_original();
+		}
+		return target;
+	}
+	
+	//説明：起点から始祖までのTreeオブジェクトリストのセッター
+	//引数：なし
+	//戻値：なし	
+	set_route(){
+		Tree.route.push(this);
+		if(this.parent != null){
+			this.parent.set_route();
+		}
+	}
+
+	//説明：起点から始祖までのTreeオブジェクトリストのゲッター
+	//引数：なし
+	//戻値：Treeオブジェクトリスト	
+	get_route(){
+		Tree.route = [];
+		this.set_route();
+		return Tree.route;
+	}
+
+}
+
+
+// ■TreeObjectクラスは以下の機能を提供します
+//①リサイズ・移動可能な親子関係をもつJQueryオブジェクト
+class TreeObject extends Tree{
+	// 共通カウンタ
+	static cnt = 0;
+	// 全Treeオブジェクトのリスト
+	static list = [];
+	
+	constructor(id){
+		super();
+		this.jquery_obj = $("#"+id);
+		this.collider = new Collider()
+						.set_jquery_obj(this.jquery_obj)
+						.set_collider()
+						.set_frame()
+						.set_button();
+		TreeObject.list.push(this);
+	}
+	
+	set_jquery_obj(jquery_obj){
+		this.collider.jquery_obj = jquery_obj;
+		return this;
+	}
+	
+	set_child(t){
+		super.set_child(t);
+	}
+	
+}
+
+// ■Gridクラスは以下の機能を提供します
+//①JQueryオブジェクトの画面配置管理
+//②JQueryオブジェクトのリサイズ監視
+
+
+class Grid{
+	constructor(w,h){
+		this.w = w;
+		this.h = h;
+		this.jquery_obj_list = [];
+		this.x_list = this.set_gridX();
+		this.y_list = this.set_gridY();
+		this.body = $("body");
+		this.set_grid_event();
+		this.active = false;
+	}
+	
+	set_jquery_obj(jquery_obj){
+		let self = this;
+		this.jquery_obj_list.push(jquery_obj);
+		jquery_obj.click(function(){
+			self.active = true;
+		});
+		return this;
+	}
+	
+	set_gridX(){
+		let grid_list = [];
+		for(let i=0; i<window.innerWidth; i+=this.w){
+			grid_list.push(i);
+		}
+		return grid_list;
+	}
+	
+	set_gridY(){
+		let grid_list = [];
+		for(let i=0; i<window.innerHeight; i+=this.h){
+			grid_list.push(i);
+		}
+		return grid_list;
+	}
+	
+	set_grid_event(){
+		let self = this;
+		this.body.mouseup(function(e){
+			if (self.active == true){
+				for(let i=0; i<self.jquery_obj_list.length; i++){
+					let obj = self.jquery_obj_list[i];
+					let grid_x = obj.offset()["left"] - (obj.offset()["left"] % self.w);
+					let grid_y = obj.offset()["top"]  - (obj.offset()["top"] % self.h);
+					let grid_w = obj.width()  - (obj.width()  % self.w);
+					let grid_h = obj.height() - (obj.height() % self.h);
+					
+					//グリッド横幅
+					if (grid_w > self.w){
+						obj.width(grid_w);
+					}else{
+						obj.width(self.w * 2);
+					}
+					
+					//グリッド縦幅
+					if (grid_h > self.h){
+						obj.height(grid_h);
+					}else{
+						obj.height(self.h * 2);
+					}
+					//グリッド座標
+					obj.offset({"top":grid_y,"left":grid_x});
+				}
+			}
+		});
+	}
+}
+
